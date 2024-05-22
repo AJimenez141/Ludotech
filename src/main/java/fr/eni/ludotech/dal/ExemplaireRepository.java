f73f4ec9d5a77099c2601fed479768f5b6db39da
Pyrex141 <alex.jimenez1411@gmail.com>
2024-05-22 16:19:19 +0200

    Méthode findExemplairesNonReserves

--- src/main/java/fr/eni/ludotech/dal/ExemplaireRepository.java
+++ src/main/java/fr/eni/ludotech/dal/ExemplaireRepository.java
@@ -1,10 +1,15 @@
 package fr.eni.ludotech.dal;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.eni.ludotech.bo.ExemplaireJeu;
 
 public interface ExemplaireRepository extends JpaRepository<ExemplaireJeu, Integer>{
 
-	
+	//Trouver un exemplaire qui n'a pas été réservé par un client
+	@Query("SELECT e FROM ExemplaireJeu e WHERE e.reservationClient IS NULL AND e.dateReservation IS NULL")
+	List<ExemplaireJeu> findExemplairesNonReserves();
 }
