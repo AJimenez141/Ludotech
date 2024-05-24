79d9c515a40bd1c9f483f0945ce80c937373b6c8
Salhigan <87065053+Salhigan@users.noreply.github.com>
2024-05-24 14:00:20 +0200

    feat creation fichier REST

---
+++ src/main/java/fr/eni/ludotech/rest/ExemplaireRestController.java
@@ -0,0 +1,34 @@
+package fr.eni.ludotech.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.eni.ludotech.bll.LocationService;
+import fr.eni.ludotech.bo.ExemplaireJeu;
+import fr.eni.ludotech.dal.ClientRepository;
+import fr.eni.ludotech.dal.ExemplaireJeuRepository;
+
+@RestController
+public class ExemplaireRestController {
+	@Autowired
+	private ClientRepository clientRepo;
+
+	@Autowired
+	private ExemplaireJeuRepository exemplaireRepo;
+
+	@Autowired
+	private LocationService locationService;
+	
+	@PutMapping("/exemplaires/reserver/{id}")
+	public ExemplaireJeu modifierExemplaire(@PathVariable(value = "id") Integer id, @RequestBody ExemplaireJeu exemplaire) {
+		exemplaire = locationService.modifierExemplaire(exemplaire);
+		return exemplaire;
+		
+	}
+}
